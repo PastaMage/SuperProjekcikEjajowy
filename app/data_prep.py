eb69db645f40c784d4b1e04c76eee2a52789bf52
PastaMage <mbebak04@gmail.com>
2026-06-23 17:03:52 +0200

    fix data prep pipeline

--- app/data_prep.py
+++ app/data_prep.py
@@ -1,33 +1,32 @@
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 import pandas as pd
 
 NUM_COLS = [
     'age', 'daily_social_media_hours', 'sleep_hours', 'screen_time_before_sleep',
     'academic_performance', 'physical_activity', 'stress_level', 'anxiety_level', 'addiction_level'
 ]
+ORDINAL_COLS = ['social_interaction_level']
+NOMINAL_COLS = ['gender', 'platform_usage']
 
-def prep_data(df: pd.DataFrame):
-    df = change_data_to_numeric(df)
+def prep_data_pipeline(df: pd.DataFrame):
+
+    social_map = {'low': 0, 'medium': 1, 'high': 2}
+    for col in ORDINAL_COLS:
+        df[col] = df[col].map(social_map)
+
+    for col in NOMINAL_COLS:
+        df = pd.get_dummies(df, columns=[col], drop_first=True)
+
     X = df.drop('depression_label', axis=1)
     y = df['depression_label']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    X_train, X_test = scale_numerics(X_train, X_test)
-    return X_train, X_test, y_train, y_test
 
-def scale_numerics(X_train, X_test):
     scaler = StandardScaler()
-
-    X_train[NUM_COLS] = scaler.fit_transform(X_train[NUM_COLS])
-    X_test[NUM_COLS] = scaler.transform(X_test[NUM_COLS])
-    print(f"Scale Numerics X_train:\n{X_train[0:5]}\nScale Numerics X_test:\n{X_test[0:5]}")
-
-    return {"X_train": X_train, "X_test": X_test}
-
-def change_data_to_numeric(df: pd.DataFrame):
-    social_map = {'low': 0, 'medium': 1, 'high': 2}
-    df['social_interaction_level'] = df['social_interaction_level'].map(social_map)
-
-    df = pd.get_dummies(df, columns=['gender', 'platform_usage'], drop_first=True)
-    print(f"Data to Numeric:\n{df[0:5]}")
-    return df
+    
+    X[NUM_COLS] = scaler.fit_transform(X[NUM_COLS])
+    
+    print(f"Dane przygotowane ręcznie. Kształt X: {X.shape}")
+    print(f"Przykładowe dane:\n{X.head(3)}")
+    
+    return X, y
--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 import pandas as pd
 from plot_generation import plot_depression_balance, plot_numeric_distributions, plot_platform_usage
-from data_prep import scale_numerics, change_data_to_numeric, prep_data
+from data_prep import prep_data_pipeline
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+import numpy as np
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import classification_report, confusion_matrix
 
 DATA_PATH = Path("./Teen_Mental_Health_Dataset.csv")
 
@@ -16,8 +22,30 @@ def load_data(path: Path) -> pd.DataFrame:
 
 def main() -> None:
     df = load_data(DATA_PATH)
-    X_train, X_test, y_train, y_test = prep_data(df)
-    
+    X, y = prep_data_pipeline(df)
+
+    forest_clf = RandomForestClassifier(class_weight='balanced', random_state=42)
+
+    # forest_scores = cross_val_score(forest_clf, X_train, y_train, cv=5, scoring='accuracy')
+
+    # print(f"Regresja Logistyczna - średnia trafność CV: {np.mean(log_scores):.4f}")
+    # print(f"Las Losowy - średnia trafność CV: {np.mean(forest_scores):.4f}")
+
+    # y_train_pred_log = cross_val_predict(log_reg, X_train, y_train, cv=5)
+    # y_train_pred_forest = cross_val_predict(forest_clf, X_train, y_train, cv=5)
+
+    # print("=== REGRESJA LOGISTYCZNA ===")
+    # print("Macierz pomyłek:")
+    # print(confusion_matrix(y_train, y_train_pred_log))
+    # print("\nRaport klasyfikacji:")
+    # print(classification_report(y_train, y_train_pred_log))
+
+    # print("\n=== LAS LOSOWY ===")
+    # print("Macierz pomyłek:")
+    # print(confusion_matrix(y_train, y_train_pred_forest))
+    # print("\nRaport klasyfikacji:")
+    # print(classification_report(y_train, y_train_pred_forest))
+
 
 if __name__ == "__main__":
-    main()
+    main()  
